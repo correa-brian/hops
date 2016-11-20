7c0299267614c7101e6eadb718d9c9d85aca30b9
brian <briancorrea@gmail.com>
2016-11-20 10:15:47 -0500

    Creating signup Vc

--- Hops/HPSignUpViewController.swift
+++ Hops/HPSignUpViewController.swift
@@ -8,17 +8,86 @@
 
 import UIKit
 
-class HPSignUpViewController: HPViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+class HPSignUpViewController: HPViewController, UITextFieldDelegate {
+    
+    var textFields = [UITextField]()
+    
+    override func loadView(){
         let frame = UIScreen.main.bounds
         let view = UIView(frame: frame)
         view.backgroundColor = .green
         
+        let width = frame.size.width
+        let height = frame.size.height
+        
+        let cancelBtn = UIButton(type: .custom)
+        cancelBtn.frame = CGRect(x: 10, y: 15, width: 32, height: 32)
+        cancelBtn.backgroundColor = .orange
+        cancelBtn.addTarget(self, action: #selector(HPViewController.exitModal), for: .touchUpInside)
+        view.addSubview(cancelBtn)
+        
+        let fieldNames = ["Email", "Password"]
+        var y = height * 0.3
+        
+        for text in fieldNames{
+            let field = UITextField(frame: CGRect(x: width*0.5-100, y: y, width: 200, height: 50))
+            field.placeholder = text
+            field.delegate = self
+            field.backgroundColor = .white
+            field.textColor = .black
+            
+            let isPassword = (text == "Password")
+            field.isSecureTextEntry = (isPassword)
+            field.returnKeyType = (isPassword) ? .done : .next
+            
+            view.addSubview(field)
+            self.textFields.append(field)
+            y += field.frame.size.height+20
+        }
+        
         self.view = view
     }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print("textFieldShouldReturn")
+        
+        let index = self.textFields.index(of: textField)!
+        
+        if index == self.textFields.count - 1{
+            var missingValue = ""
+            var profileInfo = Dictionary<String, AnyObject>()
+            
+            for field in self.textFields{
+                if field.text?.characters.count == 0{
+                    missingValue = field.placeholder!
+                    break
+                }
+                print("missing value: \(missingValue)")
+                profileInfo[field.placeholder!.lowercased()] = field.text! as AnyObject?
+            }
+            
+            if missingValue.characters.count > 0{
+                let msg = "You forgot your "+missingValue
+                let alert = UIAlertController(title: "Missing Value", message: msg, preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+                
+                self.present(alert, animated: true, completion: nil)
+                return true
+            }
+            
+            print("Profile Info: \(profileInfo)")
+            return true
+        }
+        
+        let nextField = self.textFields[index+1]
+        nextField.becomeFirstResponder()
+        return true
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
